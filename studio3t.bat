1bd28984ffea2679c51a9065f2216cd3646b2d44
Administrator <1134133509@qq.com>
2019-04-17 20:33:00 +0800

    mqtt

---
+++ studio3t.bat
@@ -0,0 +1,6 @@
+@echo off
+
+FOR /f "tokens=1,2,* " %%i IN ('reg query "HKEY_CURRENT_USER\Software\JavaSoft\Prefs\3t\mongochef\enterprise" ^| find /V "installation" ^| find /V "HKEY"') DO ECHO yes | reg add "HKEY_CURRENT_USER\Software\JavaSoft\Prefs\3t\mongochef\enterprise" /v %%i /t REG_SZ /d ""
+
+pause>nul
+exit
